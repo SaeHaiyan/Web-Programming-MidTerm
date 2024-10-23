dcb0339cba7390c3ddf116b497401c483503b4d9
SaeHaiyan <hazrinhaiyan987@gmail.com>
2024-10-23 21:50:36 +0700

    Update Icon Links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -240,9 +240,9 @@
                     <div class="footer-section social">
                         <h5>Follow Us</h5>
                         <div class="social-icons">
-                            <a href="#"><i class="fab fa-facebook"></i></a>
-                            <a href="#"><i class="fab fa-twitter"></i></a>
-                            <a href="#"><i class="fab fa-instagram"></i></a>
+                            <a href="https://www.facebook.com/login/"><i class="fab fa-facebook"></i></a>
+                            <a href="https://x.com/i/flow/login"><i class="fab fa-twitter"></i></a>
+                            <a href="https://www.instagram.com/accounts/login/?hl=en"><i class="fab fa-instagram"></i></a>
                         </div>
                     </div>
                 </div>
